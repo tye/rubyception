f53203419bd84f50cbeeb438685738d51b9ea888
tye <tyeshavik@gmail.com>
2012-11-30 12:39:14 -0500

    Fix sass initializer

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,4 +1,3 @@
 Sass::Engine::DEFAULT_OPTIONS[:load_paths].tap do |load_paths|
-  load_paths << "#{Rubyception::Engine.root}/app/assets/stylesheets"
-  load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
+  load_paths << Rubyception::Engine.root.join('app','assets','stylesheets')
 end
